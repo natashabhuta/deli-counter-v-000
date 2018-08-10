eb77245bead0e8f5fbd6e72cca09a454aec6352a
natashabhuta <natasha.bhuta@gmail.com>
2018-08-10 14:23:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 # Write your code here.
 
-katz_deli = []
+
 
 def line(katz_deli)
   katz_deli = [];
-  if katz_deli.empty? 
-     puts "The line is currently empty."
+  katz_deli.empty ? puts "The line is currently empty."
    else 
      number = 1 
    end 
